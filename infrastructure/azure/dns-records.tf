8174e62ba8d5ceb3cf5cbdc47fdb8f205428e91f
Arda Cetinkaya <ardacetinkaya@gmail.com>
2021-07-01 14:56:03 +0000

    DNS record fix

--- infrastructure/azure/dns-records.tf
+++ infrastructure/azure/dns-records.tf
@@ -1,5 +1,5 @@
 resource "azurerm_dns_a_record" "k8s_cluster_dns_zone_a_record" {
-  name                = "a-record"
+  name                = "www"
   zone_name           = azurerm_dns_zone.k8s_cluster_dns_zone_public.name
   resource_group_name = azurerm_resource_group.k8s_demo_resources.name
   ttl                 = 300
